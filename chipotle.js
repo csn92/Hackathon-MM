fd62b576b88056b34d03d5416650fbd3570f32b7
csn92 <cs3315@columbia.edu>
2016-03-14 08:18:51 -0700

    Update chipotle.js

--- chipotle.js
+++ chipotle.js
@@ -7,10 +7,10 @@ ChipsAndGuac.getNearbyLocations("10018").then(function(locations) {
 
 // instantiate a new ChipsAndGuac object, passing in required configuration and credentials. 
 var cag = new ChipsAndGuac({
-  email:'chithrasrinivasan92@gmail.com', 
-  password:'Hocuspocus', 
-  locationId: '482', 
-  phoneNumber:'917.912.8767' // must match user profile 
+  email:'', 
+  password:'', 
+  locationId: '', 
+  phoneNumber:'' // must match user profile 
 });
 
 cag.getOrders().then(function(orders) {
